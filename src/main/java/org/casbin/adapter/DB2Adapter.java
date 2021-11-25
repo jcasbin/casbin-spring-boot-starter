14b7f6bb14c3e5bb9ab4b94f2a61425ca248cfe0
饭 <liuyun105@126.com>
2021-11-25 12:10:34 +0800

    fix: db2 adapter issue (#50)
    * fix db2 adaptor issue
    
    issue: jcasbin#47
    
    
    Signed-off-by: Macky <liuyunsh@cn.ibm.com>
    
    * fix sql issue and sonar issue for db2adptor
    
    fix https://github.com/jcasbin/casbin-spring-boot-starter/issues/47
    
    Signed-off-by: Macky <liuyunsh@cn.ibm.com>
    
    * update node version from 12 to 16
    
    issue: https://github.com/jcasbin/casbin-spring-boot-starter/runs/4318955733?check_suite_focus=true
    
    Signed-off-by: Macky <liuyunsh@cn.ibm.com>
    
    Co-authored-by: Macky <liuyunsh@cn.ibm.com>

--- src/main/java/org/casbin/adapter/DB2Adapter.java
+++ src/main/java/org/casbin/adapter/DB2Adapter.java
@@ -13,17 +13,18 @@ public class DB2Adapter extends JdbcAdapter {
 
     private static final String CHECK_TABLE_SQL = "select 1 from syscat.tables where tabname = upper('CASBIN_RULE')";
 
-    private final static String DROP_TABLE_SQL = "DROP TABLE CASBIN_RULE";
+    private static final  String DROP_TABLE_SQL = "DROP TABLE CASBIN_RULE";
 
     private static final String INIT_TABLE_SQL = "CREATE TABLE CASBIN_RULE (" +
-            "  ID int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, "+
+            "  ID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) ,"+
             "  PTYPE VARCHAR(255) NOT NULL ," +
             "  V0 VARCHAR(255) DEFAULT NULL ," +
             "  V1 VARCHAR(255) DEFAULT NULL ," +
             "  V2 VARCHAR(255) DEFAULT NULL ," +
             "  V3 VARCHAR(255) DEFAULT NULL ," +
             "  V4 VARCHAR(255) DEFAULT NULL ," +
-            "  V5 VARCHAR(255) DEFAULT NULL" +
+            "  V5 VARCHAR(255) DEFAULT NULL ," +
+            "  PRIMARY KEY (ID) " +
             ")";
 
     /**
